5ce68175b8ff88ec61d86ecbb61d9da1ca2242ac
Ruben Gil <gil.r419@gmail.com>
2017-01-04 15:50:40 -0500

    made changes to server/index.js

--- back/server/index.js
+++ back/server/index.js
@@ -3,17 +3,13 @@ const app = express();
 const path = require('path');
 const applyExpressMiddleware = require('./middleware');
 const routes = require('./routes');
-const db = require('../models');
 
 applyExpressMiddleware(app);
 app.use('/api', routes);
 
-db.sequelize.sync(() => {
-  
-    //return our react app for all non-API routes
-    app.get('/*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../../', 'front/index.html'));
-    });
-})
+//return our react app for all non-API routes
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../', 'front/index.html'));
+});
 
 module.exports = app;
