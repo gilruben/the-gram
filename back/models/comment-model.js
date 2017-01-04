2c8c48d7ff7c32948b26341c8acea47b476918b2
Carlos Martinez <martinezcj2@gmail.com>
2017-01-04 16:40:21 -0500

    created comment model close #4

--- back/models/comment-model.js
+++ back/models/comment-model.js
@@ -1,8 +1,26 @@
 'user strict';
 
 module.exports = function(sequelize, DataTypes){
-  const Comment = sequelize.define("Comment", {
+  const Comment = sequelize.define("comment", {
     comment: DataTypes.STRING
+},
+  {
+    classMethods: {
+      associate: function(models){
+        Comment.belongsTo(models.user, {
+          onDelete: "CASCADE",
+          foreignKey: {
+            allowNull: false
+          }
+        }),
+        Comment.belongsTo(models.post, {
+          onDelete: "CASCADE",
+          foreignKey: {
+            allowNull: false
+          }
+        })
+      }
+    }
   })
-  return Comment;
-};
+    return Comment;
+  };
