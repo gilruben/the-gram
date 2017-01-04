c13ef556c91944744def60603ec535ef7bc1e25e
Ruben Gil <gil.r419@gmail.com>
2017-01-04 15:27:06 -0500

    synced tables in index.js of server folder

--- back/server/index.js
+++ back/server/index.js
@@ -3,13 +3,17 @@ const app = express();
 const path = require('path');
 const applyExpressMiddleware = require('./middleware');
 const routes = require('./routes');
+const db = require('../models');
 
 applyExpressMiddleware(app);
 app.use('/api', routes);
 
-//return our react app for all non-API routes
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../', 'front/index.html'));
-});
+db.sequelize.sync(() => {
+  
+    //return our react app for all non-API routes
+    app.get('/*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../../', 'front/index.html'));
+    });
+})
 
 module.exports = app;
