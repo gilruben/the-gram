daaf859243c0c95920be97f3f51d0584533080ed
Ruben Gil <gil.r419@gmail.com>
2017-01-08 17:37:27 -0500

    Delete App component

--- front/components/index.js
+++ front/components/index.js
@@ -1,7 +1,6 @@
-import App from './app/App.jsx';
 import Home from './home/Home.jsx';
 import Login from './login/Login.jsx';
 import Routes from './routes/Routes.jsx';
 import Navbar from './navbar/Navbar.jsx';
 
-export {App, Home, Login, Routes, Navbar};
+export {Home, Login, Routes, Navbar};
--- front/components/routes/Routes.jsx
+++ front/components/routes/Routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
-import {App, Home} from '../index';
+import {Home} from '../index';
 import LoginOrFeed from '../../containers/LoginOrFeed';
 import store from '../../store/store';
 import {verifyUser} from '../../route-utils';
