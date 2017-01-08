978e6767d9d26ebe9eefa5c2a9adc38341e1c473
Ruben Gil <gil.r419@gmail.com>
2017-01-07 19:49:02 -0500

    Add provider to Routes component

--- front/components/routes/Routes.jsx
+++ front/components/routes/Routes.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import {App, Home, Login} from '../index';
+import LoginContainer from '../../containers/LoginContainer';
+import store from '../../store/store';
 
 const Routes = (props) => (
-  <Router history={browserHistory}>
-    <Route path='/' component={App} >
-      <IndexRoute component={Home} />
-      <Route path='/login' component={Login} />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={Home} />
+        <Route path='/login' component={Login} />
+      </Route>
+    </Router>
+  </Provider>
 );
 
 export default Routes;
