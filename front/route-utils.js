991c8c0a4590ed980f8a9d682b07975791a96b99
Ruben Gil <gil.r419@gmail.com>
2017-01-09 13:59:26 -0500

    Remove unnecessary white space form route-utils

--- front/route-utils.js
+++ front/route-utils.js
@@ -3,13 +3,11 @@ import store from './store/store';
 import {verify} from './actions/verify-actions';
 
 export const verifyUser = (nextState, replace, cb) => {
-
   $.ajax({
     url: '/auth/verify',
     type: 'GET'
   })
   .done((data) => {
-
     store.dispatch(verify({isVerified: true}));
     cb()
   })
