8d4efcd6d5f79843376f3b578a0525baa6b66bf7
Ruben Gil <gil.r419@gmail.com>
2017-01-10 14:12:16 -0500

    Add a space in comment-router

--- back/routes/comment-router.js
+++ back/routes/comment-router.js
@@ -3,9 +3,6 @@ const User = require('../models').user;
 const Post = require('../models').post;
 const Comment = require('../models').comment;
 
-//Create GET and POST route to retrieve and create comments
-//Create GET route to retrieve a specific post's comments
-//Create GET route to retrieve a specific user's comments
 
 const getAllComments = (req, res) => {
   Comment.findAll()
