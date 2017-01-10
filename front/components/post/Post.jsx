df085d397f9030fe0fc9e57a93a1f25aea5f5853
bigkangtheory <lisalkang@gmail.com>
2017-01-10 15:04:33 -0500

    Uncomment CommentContainer from Post component

--- front/components/post/Post.jsx
+++ front/components/post/Post.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 //import LikeContainer from './LikeContainer.jsx'; from DEE
-//import CommentContainer from './CommentContainer.jsx'; from CARMAR
+import CommentContainer from '../../containers/CommentContainer';
 
 const Post = (props) => (
 	<div>	
 		<img src={props.post.img_path} />
-		<LikeContainer id={props.post.id} />
 		<CommentContainer id={props.post.id} />
 	</div>
 )
 
 export default Post;
+
+		// <LikeContainer id={props.post.id} />
