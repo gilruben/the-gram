063748eda63ec11b3a818135a71d143be12c66e7
Dee Sterling <darlenesterling@gmail.com>
2017-01-11 15:34:11 -0500

    Merge with master

--- front/containers/LikeContainer.js
+++ front/containers/LikeContainer.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {getPostsAsync} from '../actions/like-actions';
 
 const LikeContainer = React.createClass({
-<<<<<<< HEAD
   getInitialState(){
     return {likes: 0, wasLiked: false}
   },
@@ -28,32 +24,8 @@ const LikeContainer = React.createClass({
       <div onClick={likePost}>
         {this.state.likes}
       </div>
-
-=======
-  componentDidMount(){
-    this.props.getLikes()
-  },
-  render(){
-    return (
-      <div>
-          {this.props.likes.count}
-      </div>
->>>>>>> master
     )
   }
 })
 
-<<<<<<< HEAD
 export default LikeContainer;
-=======
-const mapStateToProps = (state) => {
-  console.log(state.likes)
-  return {likes: state.likes}
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({getLikes: getLikesAsync}, dispatch)
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(LikeContainer);
->>>>>>> master
