6d5d29c6bd7a85fb849e02cd47468a295266d9d9
bigkangtheory <lisalkang@gmail.com>
2017-01-12 10:44:08 -0500

    Add heart icon to comment creating box

--- front/components/comment/CreateComment.jsx
+++ front/components/comment/CreateComment.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import commentLike from '../../icons/commentLike.png';
+import igSprites from '../../icons/igSprites.png';
+
+//show the input and button horizontal
 
 const CreateComment = (props) => (
   <div>
-    COMMENT HERE
+    <div className='commentCreate'>
+    	<span className='igSpriteHeartOpen'>Like</span>
+	      <input className='commentInput' type="input" placeholder="Add a comment...">
+	      </input>
+	      <button type="submit">Send</button>
+    </div> 
+
   </div>
 )
 
